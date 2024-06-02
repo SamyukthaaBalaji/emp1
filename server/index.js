3f2e05cc767d092593cc91e3d3f54f6e21cd2882
SamyukthaaBalaji <115622106+SamyukthaaBalaji@users.noreply.github.com>
2024-06-02 22:52:08 +0530

    commiting

--- server/index.js
+++ server/index.js
@@ -1,27 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.use(express.json());
 app.use(cors());
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: process.env.EMAIL_ADDRESS,
-//         pass: process.env.EMAIL_PASS,
-//     },
-// });
-
-
 
 // Define your routes here---->login and register route
 app.use("/auth", require("./routes/jwtauth"));
-app.use("/dashboard",require("./routes/dashboard"))
-app.use("/user",require("./routes/user"))
-
+app.use("/dashboard", require("./routes/dashboard"));
+app.use("/user", require("./routes/user"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
